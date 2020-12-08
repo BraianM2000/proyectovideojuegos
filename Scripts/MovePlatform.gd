18bc260bf5c3f91faa638158f8a8180188dff6e4
Braian <Braian.morales@correounivalle.edu.co>
2020-12-08 17:10:58 -0500

    modifica el nivel

---
+++ Scripts/MovePlatform.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+export(String) var direction
+export(int) var distance
+
+const UNIT_SIZE = 96
+const IDLE_DURATION = 1.0
+
+var move_to = 0
+var speed = 3.0
+var follow = Vector2.ZERO
+onready var platform = $Platform
+onready var tween = $MoveTween
+
+func _ready():
+	if direction == "LEFT":
+		move_to = Vector2.LEFT * distance
+	if direction == "RIGHT":
+		move_to = Vector2.RIGHT * distance
+	if direction == "UP":
+		move_to = Vector2.UP * distance
+	_init_tween()
+
+func _init_tween():
+	var duration = move_to.length() / float(speed * UNIT_SIZE)
+	tween.interpolate_property(self, "follow", follow, move_to, duration,tween.TRANS_LINEAR,tween.EASE_IN_OUT,IDLE_DURATION)
+	tween.interpolate_property(self, "follow", move_to, follow, duration, Tween.TRANS_LINEAR,tween.EASE_IN_OUT,duration + IDLE_DURATION)
+	tween.start()	
+
+func _physics_process(delta):
+	platform.position = platform.position.linear_interpolate(follow, 0.075)
+
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
+export(float) var GRAVITY = 20
+export(float) var SPEED = 180
+export(float) var JUMPFORCE = 500
+
 const UP = Vector2(0, -1)
-const GRAVITY = 20
-const SPEED = 170
-const JUMPFORCE = 450
 var FACING_RIGHT = true
 var motion = Vector2()
 
@@ -11,7 +12,6 @@ var motion = Vector2()
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
-
 	
 	if FACING_RIGHT:
 		$Sprite.scale.x = 1
@@ -33,6 +33,7 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMPFORCE
+	
 	
 	motion = move_and_slide(motion, UP)
 	
